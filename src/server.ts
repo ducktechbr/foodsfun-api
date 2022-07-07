c4ebe430deb751bc8933c824aa5f7aebecd3a16d
Vitor Santos <vitor93gs@gmail.com>
2022-07-07 10:24:10 -0300

    comentado cors para teste de prog

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import { routes } from "./routes/routes";
 import { config } from "./config/config";
 
 const app = express();
-app.use(cors({ origin: process.env.NEXT_APP_URL }));
+// app.use(cors({ origin: process.env.NEXT_APP_URL }));
 app.use(express.json()); // get - buscar informaçoes , post - cadastrar informaçoes, put - atualizar informaçoes, delete - deletar informaçoes, patch - atualizar parcialmente
 
 app.use(routes);
