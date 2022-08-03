63af766bf14f39f968a853404dca655d71c7dfea
Vitor Santos <vitor93gs@gmail.com>
2022-08-03 10:03:09 -0300

    criada rota de criar ordem em uma nova comanda, e criar nova ordem em uma comanda ja existente

--- build/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.config = void 0;
-var PORT = process.env.PORT;
-exports.config = {
-    server: { port: PORT }
-};
-//# sourceMappingURL=config.js.map
